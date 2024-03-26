c8e9d5e7a31e4d2930b14f7dc57ec537fafa492e
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2024-03-26 16:14:05 +0300

    init

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import 'uno.css';
+  import 'carbon-components-svelte/css/white.css';
+  import './global.scss';
+</script>
+
+<slot />
---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div>
+  <p class="bg-red text-green">hi</p>
+</div>
