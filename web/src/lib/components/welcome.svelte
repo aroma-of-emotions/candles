afa59ef2d4de3903a02eccec8bf80cf550395576
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2024-06-11 15:38:36 +0300

    fix

--- web/src/lib/components/welcome.svelte
+++ web/src/lib/components/welcome.svelte
@@ -3,7 +3,7 @@
   import Button from './Button.svelte';
 </script>
 
-<div id="welcome" class="h-screen grid grid-cols-3 items-center pl-30 py-20">
+<div id="welcome" class="min-h-screen grid grid-cols-3 items-center pl-30 py-20">
   <div>
     <h1 class="font-bold mb-6">Вдохните искусство!</h1>
     <p class="mb-8 w-70%">
